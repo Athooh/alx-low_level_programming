9c8df3ac6fc1425fdd26fe7f49963b1114f86617
Seth Athooh <oathooh@gmail.com>
2023-07-13 14:56:08 -0700

    printing a line without using printf or puts function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main - program prints a string without using printf or puts function
+  *Return: 0 signifies Success
+  */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
