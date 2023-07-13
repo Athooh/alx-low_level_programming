9189aa998f5c298938f970ac81f28ed5a2c10b31
Seth Athooh <oathooh@gmail.com>
2023-07-13 13:22:14 -0700

    Using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - the program uses printf function to output a string
+  *Return: This returns 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
