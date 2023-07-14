21378987c65d6a0f037cbf5cf34ed4b8c4ea03ca
odhiambo_athooh <s.athooh@gmail.com>
2023-07-14 22:27:49 +0300

    variables if else while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digit numbers.
+ * Return: if 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
