b94623000a4c18d2b7a141a6da0035cb2b543972
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2023-08-08 03:13:55 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,42 +2,42 @@
 #include "main.h"
 
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
- * Return: number of words
+ * count_word - this is a helper function to count the number of words in a string
+ * @y: a string to be evaluated
+ * Return: returns the number of words
  */
 
-int count_word(char *s)
+int count_word(char *y)
 {
-	int flag, c, w;
+	int flag, c, i;
 
 	flag = 0;
-	w = 0;
+	i = 0;
 
-	for (c = 0; s[c] != '\0'; c++)
+	for (c = 0; y[c] != '\0'; c++)
 	{
-		if (s[c] == ' ')
+		if (y[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
 			flag = 1;
-			w++;
+			i++;
 		}
 	}
-	return (w);
+	return (i);
 }
 
 /**
- * **strtow - splits a string into words
- * @str: string to split
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * **strtow - it splits a string into words
+ * @str: this is the string to be split
+ * Return: it returns a pointer to an array of strings if Successful
+ * or NULL if not
  */
 
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
+	int j, l = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
 		len++;
@@ -49,26 +49,26 @@ char **strtow(char *str)
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len; i++)
+	for (j = 0; j <= len; j++)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		if (str[j] == ' ' || str[j] == '\0')
 		{
 			if (c)
 			{
-				end = i;
+				end = j;
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
+				matrix[l] = tmp - c;
+				l++;
 				c = 0;
 			}
 		}
 		else if (c++ == 0)
-			start = i;
+			start = j;
 	}
 	matrix[k] = NULL;
 	return (matrix);
