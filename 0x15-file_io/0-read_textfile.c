3bb3d88f38ac7e85b94f043c3f91a7a4332e2578
odhiambo_athooh <s.athooh@gmail.com>
2023-10-02 10:29:30 +0300

    file input initial commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- It reads textfiles printed to STDOUT.
+ * @filename: the text file to be read
+ * @letters: the number of letters read
+ * Return: returns the w- actual number of bytes read and printed
+ *        returns 0 when the function fails or filename is NULL.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t ft;
+	ssize_t n;
+	ssize_t x;
+
+	ft = open(filename, O_RDONLY);
+	if (ft == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	x = read(ft, buf, letters);
+	n = write(STDOUT_FILENO, buf, x);
+
+	free(buf);
+	close(ft);
+	return (n);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - It appends text at the end of a file.
+ * @filename: the pointer to the filename.
+ * @text_content: string added to the end of the file.
+ * Return: returns -1 if the function fails/filename is NULL and
+ *         If file does not exist, the user lacks write permissions
+ *         Otherwise 1 if successful.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int p, n, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	p = open(filename, O_WRONLY | O_APPEND);
+	n = write(p, text_content, len);
+
+	if (p == -1 || n == -1)
+		return (-1);
+
+	close(p);
+
+	return (1);
+}
